13873b56e6d896b674731184f93b05dd676ffe04
next36eee <41265236+next36eee@users.noreply.github.com>
2024-05-05 17:01:40 +0000

    u1

--- Hello.py
+++ Hello.py
@@ -15,9 +15,6 @@
 import streamlit as st
 import pandas as pd
 
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
