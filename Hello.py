fa8316a15ee7e55a44b143f2ab15cf4458b8ade5
next36eee <41265236+next36eee@users.noreply.github.com>
2024-05-05 16:25:28 +0000

    update1

--- Hello.py
+++ Hello.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import streamlit as st
+import pandas as pd
+
 from streamlit.logger import get_logger
 
 LOGGER = get_logger(__name__)
@@ -25,27 +27,13 @@ def run():
     )
 
     st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
-
+    
+    st.write("Here's our first attempt at using data to create a table:")
+    st.write(pd.DataFrame({
+        'first column': [1, 2, 3, 4],
+        'second column': [10, 20, 30, 40]
+    }))
+ 
 
 if __name__ == "__main__":
     run()
