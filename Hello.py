5c62478aac36022dcf1e4290616997c14be5a74b
next36eee <41265236+next36eee@users.noreply.github.com>
2025-01-12 20:51:32 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -104,7 +104,7 @@ st.markdown("""
 
 # Display the map
 st.markdown("""
-    <div style="margin-top:0px !important;padding:0px;">
+    <div style="margin-top:0px !important;padding:0px;align:centre;">
         <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d31019.1499631962!2d79.42148363476564!3d13.633796199999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDM4JzAxLjciTiA3OcKwMjYnMzEuNSJF!5e0!3m2!1sen!2sin!4v1736684616829!5m2!1sen!2sin" width="400" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 """, unsafe_allow_html=True)
